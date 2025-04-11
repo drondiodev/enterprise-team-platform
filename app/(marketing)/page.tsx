15d13280d454814cd659cc5498ea4971b3c0867a
drondiodev <drondiodev@icloud.com>
2025-04-11 22:02:27 +0200

    chore: clean up repository - Remove unused files and update configurations

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,8 +2,10 @@ export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <h1 className="text-4xl font-bold sm:text-6xl"></h1>
-        Welcome
+        <h1 className="text-4xl font-bold sm:text-6xl">Welcome</h1>
+        <p className="text-lg sm:text-xl">
+          This is a simple marketing page for a product or service.
+        </p>
       </main>
     </div>
   );
